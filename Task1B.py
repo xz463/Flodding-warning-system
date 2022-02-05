4df95089848236b6b370c44b0fdf1ebe0682c035
xiaorui <xz463@cam.ac.uk>
2022-02-05 19:02:30 +0000

    task 1b

---
+++ Task1B.py
@@ -0,0 +1,25 @@
+from floodsystem.geo import stations_by_distance
+from floodsystem.stationdata import build_station_list
+
+
+def run():
+    """Task 1B"""
+    #Build a list of stations
+    stations = build_station_list()
+
+    #The coordinates of Cambridge city centre
+    p = (52.2053, 0.1218)
+    print(stations_by_distance)
+
+    #Find the distance between all stations and Cambridge city centre
+    station_distance = stations_by_distance(stations, p)
+ 
+    #List only the name, town and distance of stations
+    distance_list = []
+    for (stations,distance) in station_distance:
+        distance_list.append(stations.name, stations.town, distance)
+
+    print("10 closest stations: ", distance_list[:10])
+    print("10 farthest stations: ", distance_list[-10:])
+
+run()
